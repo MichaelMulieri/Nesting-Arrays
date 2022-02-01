a73b47cf18b2079d142e00f313eb5c71926a6752
MichaelMulieri <mikemulieri@gmail.com>
2022-02-01 16:12:02 -0500

    2D array practice

---
+++ script.js
@@ -0,0 +1,39 @@
+// Could we determine if a certain value was present?
+
+function isPresent2d(arr2d, value){
+    for (var i = 0; i < arr2d.length; i++){
+        for (var j = 0; j < arr2d[i].length; j++){
+            if(arr2d[i][j] == value){
+                console.log("true");
+            }
+                else {
+                    console.log('false');
+                }
+        }
+    }
+}
+
+isPresent2d([ [1,2,3],
+              [3,7,8],
+              [9,8,6,5,4]], 6);
+
+// complete the following function
+function flatten(arr2d) {
+    var flat = [];
+    for (var i = 0; i < arr2d.length; i++){
+        for (var j = 0; j < arr2d[i].length; j++){
+            flat.push(arr2d[i][j]);
+        }
+    }
+    return flat;
+}
+    
+var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
+
+
+    
+              
+
+                
+
